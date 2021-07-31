027e66c245d71a793899d8f8fb3ef57fb23fd92f
ekobudigs <86825878+ekobudigs@users.noreply.github.com>
2021-07-31 15:13:13 +0700

    selesai basic 6

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,14 +5,15 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.asset('assets/hijab.jpg'),
-            SizedBox(
-              height: 30,
-            ),
-            Padding(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset('assets/hijab.jpg'),
+              SizedBox(
+                height: 30,
+              ),
+              Padding(
                 padding: EdgeInsets.symmetric(
                   horizontal: 30,
                 ),
@@ -48,8 +49,90 @@ class HomePage extends StatelessWidget {
                       ],
                     )
                   ],
-                )),
-          ],
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 30,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        Icon(
+                          Icons.call,
+                          color: Color(0XFF7C83FD),
+                          size: 18,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'CALL',
+                          style: TextStyle(
+                            color: Color(0XFF7C83FD),
+                            fontSize: 12,
+                          ),
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(
+                          Icons.near_me,
+                          color: Color(0XFF7C83FD),
+                          size: 18,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'CALL',
+                          style: TextStyle(
+                            color: Color(0XFF7C83FD),
+                            fontSize: 12,
+                          ),
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(
+                          Icons.share,
+                          color: Color(0XFF7C83FD),
+                          size: 18,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'CALL',
+                          style: TextStyle(
+                            color: Color(0XFF7C83FD),
+                            fontSize: 12,
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30),
+                  child: Text(
+                    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. \n\nThere are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isnt anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.',
+                    style: TextStyle(fontSize: 12),
+                    textAlign: TextAlign.justify,
+                  ))
+            ],
+          ),
         ),
       ),
     );
